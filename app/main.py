1cc0188af8badc70013ca0eaad48ab30ee1b0634
ngallot <nicolas.gallot@gmail.com>
2020-03-06 06:33:51 +0000

    add ml ping endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import logging
 
 from fastapi import FastAPI
-
+from starlette import status as sc
+from fastapi import HTTPException
 from app import __version__
 from app.config import KidsApiConfig
 
@@ -20,3 +21,18 @@ app = FastAPI(
 @app.get(path='/ping', description='Healthcheck endpoint')
 def ping():
     return 'pong'
+
+
+@app.get(path='/ml/ping', description='Machine learning endpoint healthcheck')
+def ml_ping():
+    import requests
+    ml_ping_url = f'{config.model_url}/ping'
+    logger.info(ml_ping_url)
+    response = requests.get(url=ml_ping_url)
+    if response.status_code == sc.HTTP_200_OK:
+        return 'pong'
+    else:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=response.text
+        )
