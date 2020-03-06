e3393ebd2245c92c7d64300f1fcea15f6a9ffd00
ngallot <nicolas.gallot@gmail.com>
2020-03-06 10:19:32 +0000

    add ml predictions endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import logging
 
+from typing import List, Optional
 from fastapi import FastAPI
 from starlette import status as sc
 from fastapi import HTTPException
 from app import __version__
 from app.config import KidsApiConfig
+from app.models import KidsPredictionRequest, KidsPredictionResponse
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('kids.api')
@@ -27,7 +29,6 @@ def ping():
 def ml_ping():
     import requests
     ml_ping_url = f'{config.model_url}/ping'
-    logger.info(ml_ping_url)
     response = requests.get(url=ml_ping_url)
     if response.status_code == sc.HTTP_200_OK:
         return 'pong'
@@ -36,3 +37,30 @@ def ml_ping():
             status_code=response.status_code,
             detail=response.text
         )
+
+
+@app.post(path='/ml/predictions', description='ML based predictions for Kickstarter campaign success',
+          response_model=List[KidsPredictionResponse])
+def ml_predict(requests: List[KidsPredictionRequest]) -> Optional[List[KidsPredictionResponse]]:
+    import requests as req
+    import pandas as pd
+    from fastapi.encoders import jsonable_encoder
+
+    ml_predictions_url = f'{config.model_url}/invocations'
+    ml_requests = [r.to_ml_request() for r in requests]
+    formatted_requests = pd.DataFrame.from_records(ml_requests).to_json(orient='split')
+    logger.info(formatted_requests)
+    payload = jsonable_encoder(formatted_requests)
+    ml_response = req.post(url=ml_predictions_url, data=payload,
+                        headers={'Content-type': 'application/json; format=pandas-split'})
+    if ml_response.status_code == sc.HTTP_200_OK:
+        ml_predictions = ml_response.json()
+        response_data = zip(requests, ml_predictions)
+        return [KidsPredictionResponse(prediction=prediction, **request.dict()) for request, prediction in
+                response_data]
+    else:
+        raise HTTPException(
+            status_code=ml_response.status_code,
+            detail=f'Non valid response from ml predictions endpoint. ml response: {ml_response.text}'
+        )
+
